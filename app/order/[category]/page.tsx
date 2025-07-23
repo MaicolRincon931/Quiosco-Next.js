cf68b357ca9b511713f4a8341bc03e65739cac7e
MaicolRincon931 <maicolrincon931@gmail.com>
2025-07-23 10:32:35 -0500

    nine

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -14,8 +14,11 @@ async function getProducts(category: string) {
   return products
 }
 
-export default async function OrderPage({ params }: { params: { category: string } }) {
-  const products = await getProducts(params.category)
+export default async function OrderPage({ params }: { params: Promise<{ category?: string }> }) {
+ 
+  const {category} = await params
+ 
+  const products = await getProducts(category || '')
   return (
     <>
       <Heading>
